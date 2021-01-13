d97b84b2b2808dc9d0426d35a31f33a9d7913923
minnong <kzxx1234@seoultech.ac.kr>
2021-01-13 16:31:23 +0900

    template tag update

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
 
 def post_list(request):
-    return render(request, 'blog/post_list.html' ,{})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html' , { 'posts' : posts })
+# 변수명이 정의되지 않았다면 = 를 사용하지 않았다는 것이다
 
